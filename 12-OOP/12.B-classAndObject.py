d43862105a16b50512edaf99411672086d1baa5c
Andrea Severini <andrea.severini1@vodafone.com>
2023-06-09 11:49:54 +0200

    Adding some useful explanation about memory allocation and uses

--- 12-OOP/12.B-classAndObject.py
+++ 12-OOP/12.B-classAndObject.py
@@ -58,3 +58,50 @@ student.talk()  # Calling the talk method of the student object
 # Name - Andrea
 # Age - 29
 # Marks - 28
+
+# Self and memory allocation
+
+# In Python, when an object is created from a class, memory is allocated to store the object and its attributes.
+# The self parameter in Python's class methods is a convention that refers to the object itself.
+
+# When you create an object from a class and call a method on that object, such as student.talk(),
+# the self parameter in the talk() method is automatically passed as a reference to the object
+# that the method is being called on. This allows the method to access the attributes
+# and behavior of that specific object.
+
+# Here's a breakdown of how memory allocation and the self parameter work:
+
+# Object creation:
+# When you create an object from a class, memory is allocated to store the object.
+# The memory includes space for the object's attributes and any other internal data structures required by the object.
+
+# Method call:
+# When you call a method on the object, such as student.talk(),
+# the method is executed in the context of that specific object.
+# The object is automatically passed as the first argument to the method.
+
+# self parameter:
+# The self parameter is a reference to the object itself.
+# It allows the method to access the object's attributes and perform actions using the object's data.
+# By convention, the first parameter of a method in a class is named self,
+# although you can technically choose any valid variable name.
+# However, it is recommended to stick with the convention of using self to improve code readability
+# and maintain consistency.
+
+# Accessing attributes:
+# Within the method, you can use self to access the object's attributes.
+# For example, self.name refers to the name attribute of the object, self.age refers to the age attribute,
+# and so on. The self parameter acts as a reference to the current object, allowing you to access
+# and manipulate its data.
+
+# By using the self parameter, Python ensures that each method call on an object operates on the correct instance
+# of the class.
+# It allows multiple objects of the same class to exist simultaneously, each with its own set of attributes
+# and behavior.
+# It's important to note that the self parameter is implicitly passed by the Python runtime
+# when you call a method on an object.
+# You don't need to explicitly provide the argument when calling the method.
+# Overall, the self parameter plays a crucial role in enabling object-oriented programming in Python
+# by providing a reference to the object itself within class methods.
+# It allows methods to access and manipulate the object's attributes and behavior,
+# contributing to the object's unique state and functionality.
