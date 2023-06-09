3ee05dbb4742fabe15cd0507c42088989242069f
Andrea Severini <andrea.severini1@vodafone.com>
2023-06-09 15:16:06 +0200

    updating modulus with explanations

--- 13-modules.py
+++ 13-modules.py
@@ -1,27 +1,32 @@
-from Modules import calculus
-# imported from Modules folder
+# Importing modules
+from Modules import calculus  # imported from the Modules folder
 import Modules.calculus as my_func
 from Classes.Geometry.square import Square
 from Classes.Geometry.triangle import Triangle
 
-add = calculus.add_integer(3, 5)
-print(add)
-add2 = calculus.add_integer(3, '2')
-print(add2)
-add3 = my_func.add_integer(1,2)
-print(add3)
-multiply = calculus.multiply_integer(3, 5)
-print(multiply)
-multiply2 = calculus.multiply_integer(3, '5')
-print(multiply2)
+# Using functions from the calculus module
+add = calculus.add_integer(3, 5)  # Calling the add_integer function from the calculus module
+print(add)  # Output: 8
+
+add2 = calculus.add_integer(3, '2')  # Passing mismatching types to the add_integer function
+print(add2)  # Output: Mismatching types
+
+add3 = my_func.add_integer(1, 2)  # Calling the add_integer function using the alias my_func
+print(add3)  # Output: 3
+
+multiply = calculus.multiply_integer(3, 5)  # Calling the multiply_integer function from the calculus module
+print(multiply)  # Output: 15
+
+multiply2 = calculus.multiply_integer(3, '5')  # Passing mismatching types to the multiply_integer function
+print(multiply2)  # Output: Mismatching types
 
 
 # Refactoring inheritance classes example with modules
 
-pol = Square()
-pol.set_val(10, 10)
-print(pol.area())  # Output: 100
+pol = Square()  # Creating an object of the Square class
+pol.set_val(10, 10)  # Setting the width and height of the Square object
+print(pol.area())  # Output: 100 (Calculating and printing the area of the Square)
 
-pol2 = Triangle()
-pol2.set_val(10, 10)
-print(pol2.area())  # Output: 100
+pol2 = Triangle()  # Creating an object of the Triangle class
+pol2.set_val(10, 10)  # Setting the width and height of the Triangle object
+print(pol2.area())  # Output: 100 (Calculating and printing the area of the Triangle)
